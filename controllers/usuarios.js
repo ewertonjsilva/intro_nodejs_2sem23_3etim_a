fad05c0f59e8e6e65366cacf847cf9910d97057f
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2023-10-06 11:46:04 -0300

    ajuste

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -4,7 +4,11 @@ const db = require('../database/connection');
 module.exports = {
     async listarUsuarios(request, response) {
         try {
-            return response.status(200).json({confirma: 'Listar Usuarios'});
+            const sql = 'SELECT usu_id, usu_nome, usu_email, usu_senha, usu_tipo FROM usuarios;'; 
+
+            const usuarios = await db.query(sql);
+
+            return response.status(200).json({confirma: usuarios});
         } catch (error) {
             return response.status(500).json({confirma: 'Erro', message: error});
         }
